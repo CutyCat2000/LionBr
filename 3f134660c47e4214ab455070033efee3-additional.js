adbbcec3e611876f504484c517cbc55cdfd18ebc
CutyCat2000 <132785498+CutyCat2000@users.noreply.github.com>
2023-10-16 00:16:35 +0200

    Add files via upload

--- 3f134660c47e4214ab455070033efee3-additional.js
+++ 3f134660c47e4214ab455070033efee3-additional.js
@@ -1,18 +1,14 @@
 // preload.js
 
-const remote = require('@electron/remote');
 const { contextBridge, ipcRenderer } = require('electron')
-
-const webContents = remote.getCurrentWebContents();
-
-
-window.addEventListener('keydown', (event) => {
-    if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-        event.preventDefault();
-    }
-});
 
 contextBridge.exposeInMainWorld('api', {
     send: (channel, data) => ipcRenderer.invoke(channel, data),
-    handle: (channel, callable, event, data) => ipcRenderer.on(channel, callable(event, data))
+    handle: (channel, callable, event, data) => ipcRenderer.on(channel, callable(event, data)),
+    changeTheme: (theme) => {
+      ipcRenderer.send('change-theme', theme);
+    },
+    minimize: () => {
+        ipcRenderer.send('minimize-window');
+    },
 })
--- 3f134660c47e4214ab455070033efee3-main.js
+++ 3f134660c47e4214ab455070033efee3-main.js
@@ -1,11 +1,34 @@
-const { app, BrowserWindow, globalShortcut, ipcMain } = require("electron");
+const { app, BrowserWindow, globalShortcut, ipcMain, session, nativeTheme } = require("electron");
 const path = require("path");
+const fs = require('fs');
 var _ = require('lodash');
-
+const { ElectronBlocker, requestBlocker } = require('@cliqz/adblocker-electron'); // Import ElectronBlocker and requestBlocker
+const fetch = require('cross-fetch');
 const remoteMain = require('@electron/remote/main');
 remoteMain.initialize();
+const blocker = ElectronBlocker.fromPrebuiltAdsOnly(fetch).then((blocker) => {
+    blocker.enableBlockingInSession(session.defaultSession);
+});
+function setTheme(theme) {
+  if (theme === 'dark') {
+      nativeTheme.themeSource = 'dark';
+  } else {
+      nativeTheme.themeSource = 'light';
+  }
+}
+let currentTheme = 'light';
 
 app.on("ready", () => {
+    try {
+        const configPath = path.join(__dirname, 'config.json');
+        const configData = fs.readFileSync(configPath, 'utf-8');
+        const config = JSON.parse(configData);
+        if (config.theme) {
+            currentTheme = config.theme;
+        }
+    } catch (err) {
+        console.error('Error reading config.json:', err);
+    }
     app.setName("LionBr")
     win = new BrowserWindow({
         minWidth: 600,
@@ -15,13 +38,12 @@ app.on("ready", () => {
             nodeIntegration: true,
             enableRemoteModule: true,
             preload: path.join(__dirname, './3f134660c47e4214ab455070033efee3-additional.js'),
-            devTools: false,
+            //devTools: false,
         },
         titleBarStyle: "hidden",
         titleBarOverlay: false,
         icon: path.join(__dirname, 'icon.png'),
-        allowpopups: true,
-        
+        allowpopups: true,        
     });
     remoteMain.enable(win.webContents)
     win.loadFile("./3f134660c47e4214ab455070033efee3-lionBrWebView.html");
@@ -50,11 +72,30 @@ app.on("ready", () => {
           });
         }
       });
+      
+    app.commandLine.appendSwitch('no-verify-widevine-cdm')
+    const isOffline = false
+    const widevineDir = app.getPath('userData')
+    app.on('widevine-ready', () => {
+      createWindow()
+    })
 
-      win.webContents.on("did-attach-webview", (_, contents) => {
-        contents.setWindowOpenHandler((details) => {
-          win.webContents.send('open-url', details.url);
-          return { action: 'deny' }
-        })
-      });
+    win.webContents.on("did-attach-webview", (_, contents) => {
+      contents.setWindowOpenHandler((details) => {
+        win.webContents.send('open-url', details.url);
+        return { action: 'deny' }
+      })
+    });
+    setTheme(currentTheme);
+    ipcMain.on('change-theme', (event, theme) => {
+        currentTheme = theme;
+        setTheme(theme);
+        const configPath = path.join('config.json');
+        const config = { theme };
+        fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+        console.log(`Theme changed to ${theme}`);
+    });
+    ipcMain.on('minimize-window', () => {
+        win.minimize();
+    });
 });
